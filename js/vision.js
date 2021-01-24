09d07012924daafc30e1bd20d71b4f303001a531
oumayma <oumayma@emaiketic.com>
2021-01-24 18:29:20 +0300

    init

---
+++ js/vision.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+	//options here
+    scrollHorizontally: false,
+    anchors:['our-vision', 'our-mission', 'our-value'],
+});
+
+//methods
+fullpage_api.setAllowScrolling(true);
