f3e5dd662d1470982cfb57067dbee7e15dd9203c
oumayma <oumayma@emaiketic.com>
2021-02-01 13:51:52 +0300

    fix about page

--- js/index.js
+++ js/index.js
@@ -13,13 +13,13 @@ leadership_anchor.addEventListener('mouseleave', function() {
 const leadership_anchor_mobile = document.getElementById("menu-item-leadership-for-mobile");
 
 
-// leadership_anchor_mobile.addEventListener('mouseover', function() {
-//     document.getElementById('sub-menu-list-about-for-mobile').classList.add('larger-mobile');
-// });
+leadership_anchor_mobile.addEventListener('mouseover', function() {
+    document.getElementById('sub-menu-list-about-for-mobile').classList.add('larger-mobile');
+});
 
-// leadership_anchor_mobile.addEventListener('mouseleave', function() {
-//     document.getElementById('sub-menu-list-about-for-mobile').classList.remove('larger-mobile');
-// });
+leadership_anchor_mobile.addEventListener('mouseleave', function() {
+    document.getElementById('sub-menu-list-about-for-mobile').classList.remove('larger-mobile');
+});
 
 
 let hamburger = document.querySelector('.hamburger');
