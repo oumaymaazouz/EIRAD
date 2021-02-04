2621c0d74b3aba35a4b9d97c0b685b81d99d039d
oumayma <oumayma@emaiketic.com>
2021-02-04 15:22:38 +0300

    fix desc of directors in mobile

--- js/about.js
+++ js/about.js
@@ -25,33 +25,79 @@ function manageVisibility(selectedAvatar) {
         document.getElementById(value+'-description').classList.add('visible');
         selectedAvatar.classList.add('selected-director');
         myobj.set_elemId(value);
+
+        // For mobile screens
+        // document.getElementById(value+'-description').scrollIntoView({
+        //     block: "start",
+        //     behavior: "smooth",
+        // });
+        
+      
     }
 }
+
+
+
+
+function manageVisibilityForMobile(selectedAvatar) {
+    const value = myobj.get_selected();
+    const a = myobj.get_elemId()+'-description-mobile';
+    if(!a.startsWith('-') && a !== value+'-description-mobile') {
+        document.getElementById(a).classList.remove('visible');
+        document.getElementById(myobj.get_elemId()).classList.remove('selected-director');
+    }
+    if(document.getElementById(value+'-description-mobile').classList.length == 4) {
+        document.getElementById(value+'-description-mobile').classList.remove('visible');
+        selectedAvatar.classList.remove('selected-director');
+    } else {
+        document.getElementById(value+'-description-mobile').classList.add('visible');
+        selectedAvatar.classList.add('selected-director');
+        myobj.set_elemId(value);
+    }
+}
+
+let isMobile = window.matchMedia('(max-width: 775px)');
 
 const chairman = document.getElementById("chairman");
 chairman.addEventListener('click', function() {
     myobj.set_selected('chairman');
-    manageVisibility(chairman);
+    if(isMobile) {
+        manageVisibilityForMobile(chairman)
+    } else {
+        manageVisibility(chairman);
+    }
 });
 
 const managing_director = document.getElementById("managing-director");
 managing_director.addEventListener('click', function() {
     myobj.set_selected('managing-director');
-    manageVisibility(managing_director);
+    if(isMobile) {
+        manageVisibilityForMobile(managing_director)
+    } else {
+        manageVisibility(managing_director);
+    }
 });
 
 
 const vp_inter_business = document.getElementById("vp-inter-business");
 vp_inter_business.addEventListener('click', function() {
     myobj.set_selected('vp-inter-business');
-    manageVisibility(vp_inter_business);
+    if(isMobile) {
+        manageVisibilityForMobile(vp_inter_business)
+    } else {
+        manageVisibility(vp_inter_business);
+    }
 });
 
 
 const vp_business_dev = document.getElementById("vp-business-dev");
 vp_business_dev.addEventListener('click', function() {
     myobj.set_selected('vp-business-dev');
-    manageVisibility(vp_business_dev);
+    if(isMobile) {
+        manageVisibilityForMobile(vp_business_dev)
+    } else {
+        manageVisibility(vp_business_dev);
+    }
 });
 
 
