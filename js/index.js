0cd7bceefef977e13e61e9f2d4b7dcbb078f805d
oumayma <oumayma@emaiketic.com>
2021-02-07 19:02:26 +0300

    Fixes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 let menu_about_item = document.getElementById('menu-item-about');
 let sub_menu_about = document.getElementById('sub-menu-list-about');
+let about_text = document.querySelector('.about-text');
 menu_about_item.addEventListener('click', function() {
    sub_menu_about.classList.toggle('isVisible');
+   about_text.classList.toggle('underlined')
 });
 
 let menu_about_item_mobile = document.getElementById('menu-item-about-for-mobile');
