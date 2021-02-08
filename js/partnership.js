e7eaef0c0b47a21a950cf9091fa015425cee0003
oumayma <oumayma@emaiketic.com>
2021-02-08 12:48:10 +0300

    fix partnership page

--- js/partnership.js
+++ js/partnership.js
@@ -44,11 +44,12 @@ btn_ngmsa.addEventListener('click', function() {
 });
   
 
-let btn_eirad_travel = document.querySelector('.read-more-eirad-travel');
+let btn_eirad_travel = document.querySelector('.rm-eirad-travel');
 let card_content_hidden_eirad = document.querySelector('.card-content-hidden-eirad-travel');
 
 btn_eirad_travel.addEventListener('click', function() {
     if (btn_eirad_travel.innerHTML === "Read more") {
+      console.log('.......', btn_eirad_travel.innerHTML);
       btn_eirad_travel.innerHTML = "Read less";
     } else {
       btn_eirad_travel.innerHTML = "Read more";
