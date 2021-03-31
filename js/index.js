b0317c15d89aff7c5b63104667ea279324376752
oumayma <oumayma@emaiketic.com>
2021-03-31 14:48:27 +0300

    fixes

--- js/index.js
+++ js/index.js
@@ -1,28 +1,39 @@
-let menu_about_item = document.getElementById('menu-item-about');
-let sub_menu_about = document.getElementById('sub-menu-list-about');
-let about_text = document.querySelector('.about-text');
-menu_about_item.addEventListener('click', function() {
-   sub_menu_about.classList.toggle('isVisible');
-   about_text.classList.toggle('underlined')
+let menu_about_item = document.getElementById("menu-item-about");
+let sub_menu_about = document.getElementById("sub-menu-list-about");
+let about_text = document.querySelector(".about-text");
+menu_about_item.addEventListener("click", function () {
+  sub_menu_about.classList.toggle("isVisible");
+  about_text.classList.toggle("underlined");
 });
 
-let menu_about_item_mobile = document.getElementById('menu-item-about-for-mobile');
-let sub_menu_about_mobile = document.getElementById('sub-menu-list-about-for-mobile');
-menu_about_item_mobile.addEventListener('click', function() {
-   sub_menu_about_mobile.classList.toggle('isVisibleInMobile');
+let menu_about_item_mobile = document.getElementById(
+  "menu-item-about-for-mobile"
+);
+let sub_menu_about_mobile = document.getElementById(
+  "sub-menu-list-about-for-mobile"
+);
+menu_about_item_mobile.addEventListener("click", function () {
+  sub_menu_about_mobile.classList.toggle("isVisibleInMobile");
 });
 
+let hamburger = document.querySelector(".hamburger");
+let menuOverlay = document.querySelector(".mobile-menu-overlay");
 
-let hamburger = document.querySelector('.hamburger');
-let menuOverlay = document.querySelector('.mobile-menu-overlay');
-
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('isMenuActive');
-    menuOverlay.classList.toggle('isMenuOverlayActive');  
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("isMenuActive");
+  menuOverlay.classList.toggle("isMenuOverlayActive");
 });
-  
 
 function handleShareLocationModalVisibility() {
-   document.querySelector('body').classList.toggle('body-with-opened-modal');
-   document.querySelector('.share-location-modal-overlay').classList.toggle('share-location-modal-overlay-visible');
+  document.querySelector("body").classList.toggle("body-with-opened-modal");
+  document
+    .querySelector(".share-location-modal-overlay")
+    .classList.toggle("share-location-modal-overlay-visible");
 }
+
+
+function copy(inputId) {
+   var inputElement = document.getElementById(inputId);
+   inputElement.select();
+   document.execCommand("copy");
+ }
